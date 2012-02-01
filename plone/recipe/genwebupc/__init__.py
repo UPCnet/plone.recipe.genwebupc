f2ea4dfbbe3d85d9fb9124f603b1782c4e818fa7
Víctor Fernández de Alba <victor.fernandez@upcnet.es>
2012-02-01 20:54:34 +0100

    initial

---
+++ plone/recipe/genwebupc/__init__.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+"""Recipe genwebupc"""
+
+import urllib2
+import os
+
+
+class Recipe(object):
+    """zc.buildout recipe"""
+
+    def __init__(self, buildout, name, options):
+        self.buildout, self.name, self.options = buildout, name, options
+
+    def install(self):
+        """Installer"""
+        # XXX Implement recipe functionality here
+
+        # Return files that were created by the recipe. The buildout
+        # will remove all returned files upon reinstall.
+        options = self.options
+        directory = self.buildout['buildout'].get('directory')
+        DEFAULT_CONFIG_DIR = directory + '/config'
+
+        configdir = options.get('configdir', DEFAULT_CONFIG_DIR)
+        if not os.path.exists(configdir):
+            os.mkdir(configdir)
+
+        configservertype = options.get('configservertype', 'file')
+
+        zeonames = options.get('zeonames', '').split(' ')
+        deploytype = options.get('deploytype', 'zeo')
+
+        if deploytype == 'zeo':
+
+            for zeoname in zeonames:
+
+                if configservertype == 'http':
+                    configserver = options.get('configserver', 'localhost')
+                    try:
+                        instancies = urllib2.urlopen(('http://' + configserver + '/instancies/' + zeoname), None)
+                    except:
+                        raise AssertionError('Error accessing URL: %s' % ('http://' + configserver + '/instancies/' + zeoname))
+                else:
+                    try:
+                        instancies = open(configdir + '/' + zeoname, "r")
+                    except:
+                        raise AssertionError('No zeoname config file found in %s for %s' % (configdir, zeoname))
+
+                configZeoFile = open(configdir + '/' + 'instancies-' + zeoname + ".conf", "w")
+
+                for instance in instancies.readlines():
+                    instance = instance.replace('\n', '')
+                    # Now we write the ZEO config
+                    configZeoFile.write("<blobstorage " + instance + ">\n")
+                    configZeoFile.write("  blob-dir /var/plone/genwebupcZEO/produccio/var/blobs/" + instance + "\n")
+                    configZeoFile.write("  <filestorage " + instance + ">\n")
+                    configZeoFile.write("    path " + directory + "/var/filestorage/Data_" + instance + ".fs\n")
+                    configZeoFile.write("  </filestorage>\n")
+                    configZeoFile.write("</blobstorage>\n")
+
+                configZeoFile.close()
+                instancies.close()
+
+        if deploytype == 'zope':
+
+            localhost = options.get('localhost', 'localhost')
+            import ipdb; ipdb.set_trace( )
+            ports = options.get('zeoportsmap', 'localhost')
+            try:
+                zeoportsmap = dict(zip(zeonames, ports))
+            except:
+                raise AssertionError('Config for zeonames and zeoportsmap options have not the same number of items')
+
+            # zeonames zeos = ['zeo1', 'zeo2', 'zeo3', 'zeo4', 'zeo5', 'zeo7', 'zeo8', 'zeo9', 'zeo10', 'zeo11', ]
+            # ports = {'zeo1':8001, 'zeo2':8002, 'zeo3':8003, 'zeo4':8004, 'zeo5':8005, 'zeo6':8006, 'zeo7':8007, 'zeo8':8008, 'zeo9':8009, 'zeo10':8010, 'zeo11':8011, 'zeo12':8012,}
+            # configserver = 'mebsuta.upc.es'
+
+            # Download or adquire from filesystem the Zope instances map for this frontend
+            if configservertype == 'http':
+                configserver = options.get('configserver', 'localhost')
+                try:
+                    configFE = urllib2.urlopen(('http://' + configserver + '/config/' + localhost), None)
+                except:
+                    raise AssertionError('Error accessing URL: %s' % ('http://' + configserver + '/config/ ' + localhost))
+            else:
+                try:
+                    configFE = open(configdir + '/' + localhost, "r")
+                except:
+                    raise AssertionError('No zeoname config file found in %s for %s' % (configdir, localhost))
+
+            # assignacions = {'zeo1':[], 'zeo2':[], 'zeo3':[], 'zeo4':[], 'zeo5':[], 'zeo6':[], 'zeo7':[], 'zeo8':[], 'zeo9':[], 'zeo10':[], 'zeo11':[], 'zeo12':[],}
+            # servers = {'zeo1':"", 'zeo2':"", 'zeo3':"", 'zeo4':"", 'zeo5':"", 'zeo6':"", 'zeo7':"", 'zeo8':"", 'zeo9':"", 'zeo10':"", 'zeo11':"", 'zeo12':"",}
+            # for assignacio in configFE:
+            #     assignacio = assignacio.split()
+            #     assignacions[assignacio[0]].append(assignacio[1])
+            #     servers[assignacio[0]] = assignacio[3]
+
+            zopemaps = {}
+            servers = {}
+
+            for line in configFE:
+                zeo, fe, port, be = line.split()
+                zopemap = zopemaps.setdefault(zeo, [])
+                zopemap.append(fe)
+
+                if zeo not in servers:
+                    servers[zeo] = be
+
+            for zeoname in zeonames:
+                # Download or adquire from filesystem the ZODB mount point map
+                # for this frontend and the corresponding backend
+                for zope in zopemaps[zeoname]:
+                    if configservertype == 'http':
+                        configserver = options.get('configserver', 'localhost')
+                        try:
+                            instancies = urllib2.urlopen(('http://' + configserver + '/instancies/' + zeoname), None)
+                        except:
+                            raise AssertionError('Error accessing URL: %s' % ('http://' + configserver + '/instancies/' + zeoname))
+                    else:
+                        try:
+                            instancies = open(configdir + '/' + zeoname, "r")
+                        except:
+                            raise AssertionError('No zeoname config file found in %s for %s' % (configdir, zeoname))
+
+                    zopeConfigFile = open(configdir + '/' + zope + '-mountpoints.conf', "w")
+                    self.writeZopeConfig(directory, zopeConfigFile, instancies, zeoname, servers[zeoname], zeoportsmap)
+                    zopeConfigFile.close()
+                    instancies.close()
+
+        return tuple()
+
+    def writeZopeConfig(self, directory, zopeConfigFile, instancies, zeoname, server, zeoportsmap):
+        for instance in instancies:
+            instance = instance.strip('\n')
+            zopeConfigFile.write("<zodb_db " + instance + ">\n")
+            zopeConfigFile.write("  cache-size 5000\n")
+            zopeConfigFile.write("  <zeoclient>\n")
+            zopeConfigFile.write("     blob-dir " + directory + "/var/blobs/" + instance + "\n")
+            zopeConfigFile.write("     shared-blob-dir off\n")
+            zopeConfigFile.write("     server " + server + ":" + str(zeoportsmap[zeoname]) + "\n")
+            zopeConfigFile.write("     min-disconnect-poll 1\n")
+            zopeConfigFile.write("     storage " + instance + "\n")
+            zopeConfigFile.write("     name " + instance + "\n")
+            zopeConfigFile.write("     cache-size 20MB\n")
+            zopeConfigFile.write("  </zeoclient>\n")
+            zopeConfigFile.write("  mount-point /" + instance + "\n")
+            zopeConfigFile.write("</zodb_db>\n")
+
+    def update(self):
+        """Updater"""
+        pass
---
+++ setup.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the tool of plone.recipe.genwebupc
+"""
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '1.0'
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('plone', 'recipe', 'genwebupc', 'README.txt')
+    + '\n' +
+    'Contributors\n'
+    '************\n'
+    + '\n' +
+    read('CONTRIBUTORS.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+   'Download\n'
+    '********\n')
+
+entry_point = 'plone.recipe.genwebupc:Recipe'
+entry_points = {"zc.buildout": ["default = %s" % entry_point]}
+
+tests_require = ['zope.testing', 'zope.exceptions', 'zc.buildout']
+
+setup(name='plone.recipe.genwebupc',
+      version=version,
+      description="",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        'Framework :: Buildout',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: Zope Public License',
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.recipe'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'zc.buildout'
+                        # -*- Extra requirements: -*-
+                        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite='plone.recipe.genwebupc.tests.test_docs.test_suite',
+      entry_points=entry_points,
+      )
