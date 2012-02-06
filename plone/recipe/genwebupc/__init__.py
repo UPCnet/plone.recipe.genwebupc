75f1f4d28eb9768d088e44529e2e4311a56d0974
Víctor Fernández de Alba <victor.fernandez@upcnet.es>
2012-02-06 13:36:18 +0100

    updated

--- plone/recipe/genwebupc/__init__.py
+++ plone/recipe/genwebupc/__init__.py
@@ -19,18 +19,51 @@ class Recipe(object):
         # will remove all returned files upon reinstall.
         options = self.options
         directory = self.buildout['buildout'].get('directory')
-        DEFAULT_CONFIG_DIR = directory + '/config'
+
+        DEFAULT_CONFIG_DIR = '%s/config' % directory
 
         configdir = options.get('configdir', DEFAULT_CONFIG_DIR)
         if not os.path.exists(configdir):
             os.mkdir(configdir)
+            raise AssertionError('Configuration placeholder %s did not exist (now it does) and therefore no configuration found.' % configdir)
 
         configservertype = options.get('configservertype', 'file')
 
-        zeonames = options.get('zeonames', '').split(' ')
+        zeoconfig = [c for c in options.get('zeoconfig', None).splitlines() if c]
+        if not zeoconfig:
+            raise AssertionError('No ZEO servers to configure')
+
+        zeonames = []
+        zeoportsmap = {}
+        for zeomap in zeoconfig:
+            zeomap = zeomap.split(' ')
+            if len(zeomap) != 2:
+                raise AssertionError('Config for zeoconfig options have an odd number of items. It must have two, the zeo name and its port.')
+            zeonames.append(zeomap[0])
+            zeoportsmap[zeomap[0]] = zeomap[1]
+
         deploytype = options.get('deploytype', 'zeo')
 
         if deploytype == 'zeo':
+
+            # Make sure that an active zeo part exist and have the name specified in zeomap
+            zeo_recipes = (
+                'plone.recipe.zeoserver',
+                'plone.recipe.zope2zeoserver',
+                )
+            parts = self.buildout['buildout']['parts']
+            part_names = parts.split()
+            activezeo = False
+            for part_name in part_names:
+                part = self.buildout[part_name]
+                if part.get('recipe') in zeo_recipes:
+                    activezeo = True
+                    if part_name not in zeonames:
+                        raise AssertionError('No zeoname specified for buildout part name: %s' % (part_name))
+                    if not part.get('zeo-conf-additional'):
+                        raise AssertionError('No additional mountpoint config for part name: %s' % (part_name))
+            if not activezeo:
+                raise AssertionError('No active zeo recipe part name.')
 
             for zeoname in zeonames:
 
@@ -63,12 +96,23 @@ class Recipe(object):
 
         if deploytype == 'zope':
 
+            # Make sure that an active zope part exist and have an additional config
+            zope_recipes = (
+                'plone.recipe.zope2instance',
+                )
+            parts = self.buildout['buildout']['parts']
+            part_names = parts.split()
+            activezeo = False
+            for part_name in part_names:
+                part = self.buildout[part_name]
+                if part.get('recipe') in zope_recipes:
+                    activezeo = True
+                    if not part.get('zope-conf-additional'):
+                        raise AssertionError('No additional mountpoint config for part name: %s' % (part_name))
+            if not activezeo:
+                raise AssertionError('No active zeo recipe part name.')
+
             localhost = options.get('localhost', 'localhost')
-            ports = options.get('zeoportsmap', '8001').split(" ")
-            try:
-                zeoportsmap = dict(zip(zeonames, ports))
-            except:
-                raise AssertionError('Config for zeonames and zeoportsmap options have not the same number of items')
 
             # zeonames zeos = ['zeo1', 'zeo2', 'zeo3', 'zeo4', 'zeo5', 'zeo7', 'zeo8', 'zeo9', 'zeo10', 'zeo11', ]
             # ports = {'zeo1':8001, 'zeo2':8002, 'zeo3':8003, 'zeo4':8004, 'zeo5':8005, 'zeo6':8006, 'zeo7':8007, 'zeo8':8008, 'zeo9':8009, 'zeo10':8010, 'zeo11':8011, 'zeo12':8012,}
